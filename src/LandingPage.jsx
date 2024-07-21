4a2a15236f8a276af79514a2b5e7a0b268c5df2c
Bisheshwor Khadka <bkrazz2058@gmail.com>
2024-07-21 20:53:18 +0545

    update design without responsive

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './LandingPage.css';
+
+const images = [
+    'images/1.png',
+    'images/2.JPG',
+    'images/3.png',
+    'images/4.png',
+    'images/5.JPG',
+    'images/6.JPG',
+    'images/7.jpg',
+    'images/8.jpg',
+    'images/9.jpg',
+    'images/10.jpg',
+    'images/11.jpg',
+    'images/12.jpg',
+];
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+    return (
+        <div className="landing-page">
+            <div className="pentagon-container">
+                <div className="pentagon">
+                    {images.map((image, index) => (
+                        <div key={index} className="grid-item" style={{ backgroundImage: `url(${image})` }}></div>
+                    ))}
+                </div>
+            </div>
+            <div className="text-container">
+                <h1>Prisma</h1>
+                <h2>ADVERTISEMENT</h2>
+            </div>
+        </div>
+    );
+};
 
-export default LandingPage
+export default LandingPage;
